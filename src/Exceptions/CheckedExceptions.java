7225c3d39458443af7a742bd3412356ad5191569
Ricardo Camacho <rscamachodev@gmail.com>
2022-11-05 20:15:16 -0300

    checked exceptions

--- src/Exceptions/CheckedExceptions.java
+++ src/Exceptions/CheckedExceptions.java
@@ -3,22 +3,37 @@ package Exceptions;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
+import javax.swing.JOptionPane;
 
 public class CheckedExceptions {
   public static void main(String[] args) {
-    
+    String crouchRomances = "romances-blake-crouch.txt";
+    try {
+      printFile(crouchRomances);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+      JOptionPane.showMessageDialog(null, "Verifique o nome do arquivo informado.\n" + e.getMessage());
+    } catch (IOException e) {
+      e.printStackTrace();
+      JOptionPane.showMessageDialog(null, "Outro erro de IO. Contate o suporte.\n" + e.getMessage());
+    } finally {
+      System.out.println("Esse bloco sempre será executado, ocorrendo ou não exceptions.");
+    }
+    System.out.println("Apesar da exception, o programa continua.");
   }
 
-  private static void printFile (String filename) {
+  private static void printFile (String filename) throws IOException {
     File file = new File(filename);
     BufferedReader br = new BufferedReader(new FileReader(file.getName()));
     String line = br.readLine();
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     do {
-      bw.write(line);;
+      bw.write(line);
       bw.newLine();
       line = br.readLine();
     } while (line != null);
